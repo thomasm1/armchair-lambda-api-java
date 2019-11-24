33064c6350e66119d1054846d30d1488318b4d8d
thomas maestas <thomasm1.maestas@gmail.com>
2019-11-23 19:52:09 -0500

    commit-1: plugins; pom.xml; AWS apis

---
+++ src/main/java/com/armchairbitcoinist/aws/GatewayResponse.java
@@ -0,0 +1,42 @@
+package com.armchairbitcoinist.aws;
+
+import java.util.Map;
+
+public class GatewayResponse {
+    private String body;
+    private Integer statusCode;
+    private Map<String, String> headers;
+    private boolean isBase64Encoded;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public boolean isBase64Encoded() {
+        return isBase64Encoded;
+    }
+
+    public void setBase64Encoded(boolean base64Encoded) {
+        isBase64Encoded = base64Encoded;
+    }
+}
---
+++ src/main/java/com/armchairbitcoinist/aws/LambdaJavaAPI.java
@@ -0,0 +1,5 @@
+package com.armchairbitcoinist.aws;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaJavaAPI implements RequestHandler<Object, GatewayResponse> {
+}
